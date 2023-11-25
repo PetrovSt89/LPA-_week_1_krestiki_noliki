d2d53960ce251b4439fd8dc14f14941cc298162b
Stanislav Petrov <petrovst@mail.ru>
2023-11-25 15:37:23 +0300

    исправил замечания

--- kr_nol.py
+++ kr_nol.py
@@ -1,6 +1,8 @@
 import os
 import random
 
+ALL_STEPS = [[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]]
+
 # функция очистки игры
 def clear():
     os.system('cls')
@@ -80,20 +82,28 @@ def man_step(base_turns):
 
 
 # функция хода компьютера
+# def comp_step(base_turns):
+#     all_steps = [[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]]
+#     if base_turns ==[]:
+#         turn = random.choice(all_steps)
+#         return turn
+#     else:
+#         for i in base_turns:
+#             all_steps.remove(i)
+#         # steps = [all_steps.remove(i) for i in base_turns]
+#         if all_steps == []:
+#             clear()
+#             return False
+#         turn = random.choice(all_steps)
+#         return turn
+
+# функция хода компьютера
 def comp_step(base_turns):
-    all_steps = [[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3]]
-    if base_turns ==[]:
-        steps = all_steps
-    else:
-        for i in base_turns:
-            all_steps.remove(i)
-        # steps = [all_steps.remove(i) for i in base_turns]
-        if all_steps == []:
-            clear()
-            return False
-    while True:
-        turn = random.choice(all_steps)
-        return turn
+    steps = [step for step in ALL_STEPS if step not in base_turns]
+    turn = random.choice(steps)
+    clear()
+    return turn
+        
 
 
 # функция изменения поля
@@ -115,9 +125,9 @@ def check_win(field):
         x = 'x'
         o = 'o'
         for i in field:
-            if 'x' in i and i.count('x') == 3:
+            if i.count('x') == 3:
                 return x
-            if 'o' in i and i.count('o') == 3:
+            if i.count('o') == 3:
                 return o
         return ''
 
@@ -132,7 +142,7 @@ def check_win(field):
 
 
     # переворот строки
-    def revers_str(field):
+    def revers_diag(field):
         exp_f =[]
         for i in field:
             exp_f.append(i[::-1])
@@ -152,14 +162,12 @@ def check_win(field):
 
 
     # основные проверки
-    sum_field = field + field_reverce(field) + diag(field) + revers_str(field)
+    sum_field = field + field_reverce(field) + diag(field) + revers_diag(field)
     winer = triple(sum_field)
     if winer:
         return winer
 
 def return_append_change(turn, x_o, field, base_turns):
-    if turn == False:
-        return ''
     base_turns.append(turn)
     change_field(turn, x_o, field)
 
@@ -171,7 +179,7 @@ def game_progress():
     print_field(field)
     base_turns = []
     x_o_comp, x_o_man = choise_x_o()
-    step = 'man'
+    step = 'man' if x_o_man == 'x' else 'comp'
     while True:
         if step == 'man':    
             print(f'ты играешь "{x_o_man}"')
@@ -182,11 +190,16 @@ def game_progress():
             turn = comp_step(base_turns)
             x_o = x_o_comp
             step = 'man'
+        if turn == False:
+            return ''
         return_append_change(turn, x_o, field, base_turns)
         
         winer = check_win(field)
         if winer:
             return winer, x_o_comp, x_o_man
+        if sorted(ALL_STEPS) == sorted(base_turns):
+            clear()
+            return ''
 
 
 enter_game()
